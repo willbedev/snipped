78ecfca45c8a0b3174c817755180647a13cde1c7
MarcoWillbe <marco@willbecreative.com>
2022-07-20 15:14:09 +0200

    riordino cartelle

--- gravity/filtri campi form/function.php
+++ gravity/filtri campi form/function.php
@@ -1,11 +1,11 @@
-// filtro mail 
+// filtro mail con lista domini gestiti da regex
 
 add_filter( 'gform_field_validation_4_37', 'validate_email', 10, 4 );
 function validate_email( $result, $value, $form, $field ) {
 	$pattern = "/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(arcese.com|ventanaserra.com|willbecreative.com|arcese+-+cosulich.ie)$/";
 	if ( $field->type == 'email' && $field->emailFormat != 'standard' && ! preg_match( $pattern, $value ) ) {
 		$result['is_valid'] = false;
-		$result['message'] = _('Inserire una email valida','traduzioni');
+		$result['message'] = __('Inserire una email valida','traduzioni');
 	}
 	return $result;
 }
