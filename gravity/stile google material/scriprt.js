e79f4191b6c22eae1ca5386d9a00cd13358f5056
willbedev <willbedevelop@gmail.com>
2022-11-24 09:33:26 +0100

    Update scriprt.js

--- gravity/stile google material/scriprt.js
+++ gravity/stile google material/scriprt.js
@@ -16,11 +16,11 @@ const callback = (mutationList, observer) => {
     }
   }
     for (const mutation of mutationList) {
-        jQuery('form input, form  textarea, form  select').each( function(key,element){
-            if( jQuery(element).val().length != 0 ) {
-                console.log(mutation);
-                jQuery(element).parent().siblings('label').addClass('focused');
-                // console.log( jQuery(element).parent().siblings('label'))
+        jQuery('form .wrap input, form .wrap  textarea, form .wrap  select').each( function(key,element){
+            if (typeof jQuery(element).val() === 'string') {
+                if( jQuery(element).val().length != 0 ) {
+                    jQuery(element).parent().siblings('label').addClass('focused');
+                }
             }
         })
       }
@@ -35,11 +35,23 @@ const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
 //oserver
 
+
+//focus
 jQuery(document).on('focus', 'form input, form textarea, form select', function () {
-    //console.log(jQuery(this).parent().siblings('label'));
+    // console.log(jQuery(this).parent().siblings('label'));
     jQuery(this).parent().siblings('label').addClass('focused');
-    //console.log('focusin')
+    // console.log('focusin')
 });
+
+jQuery(document).ready(function(){
+    jQuery('form .gf_readonly input, form .gf_readonly textarea, form .gf_readonly select').each( function(key,element){
+        if( jQuery(element).val().length != 0 ) {
+            jQuery(element).parent().siblings('label').addClass('focused');
+            // console.log( jQuery(element).parent().siblings('label'))
+        }
+    })
+})
+
 
 jQuery(document).on('focusout', 'form input, form textarea, form select', function () {
     var input = jQuery(this);
@@ -48,19 +60,36 @@ jQuery(document).on('focusout', 'form input, form textarea, form select', functi
         input.parent().siblings('label').removeClass('focused');
 
     }
-    //console.log('focusout')
+    console.log('focusout')
 });
 
 
+// jQuery(document).bind('gform_post_render', function () {
 
-jQuery(document).bind('gform_post_render', function () {
 
+//     jQuery('form .gfield input, form .gfield textarea, form .gfield select').each(function () {
+//         var input = jQuery(this);
 
-    jQuery('form .gfield input, form .gfield textarea, form .gfield select').each(function () {
-        var input = jQuery(this);
+//         if (jQuery(this).val().length > 0) {
+//             input.parent().siblings('label').addClass('focused');
+//         }
+//     });
+// });
 
-        if (jQuery(this).val().length > 0) {
-            input.parent().siblings('label').addClass('focused');
-        }
-    });
-});
+if(document.querySelector(".Mustread")){
+    document.querySelector(".Mustread").disabled = true;
+    document.querySelector(".Mustread .gfield_description ").addEventListener(
+        'scroll',
+        function()
+        {
+            var scrollTop = document.querySelector('.Mustread .gfield_description').scrollTop;
+            var scrollHeight = document.querySelector('.Mustread .gfield_description').scrollHeight; // added
+            var offsetHeight = document.querySelector('.Mustread .gfield_description').offsetHeight;
+            var contentHeight = scrollHeight - offsetHeight; // added
+            if (contentHeight <= scrollTop) // modified
+            {
+                document.querySelector(".Mustread").disabled = false;
+            }
+        },
+    )
+}
