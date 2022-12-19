e95022423e578a2cdd580df46f21d8465ce326af
MarcoWillbe <marco@willbecreative.com>
2022-12-19 17:00:11 +0100

    correzione codice

--- WPML/ ruolo editor per traduzoni.php
+++ WPML/ ruolo editor per traduzoni.php
@@ -1,4 +1,4 @@
-/* fu functions php abilita */
+/* dentro functions php per abilitare ruolo editor WP alle traduzioni */
 
 add_filter('wpml_user_can_translate', function ($user_can_translate, $user){
     if (in_array('editor', (array) $user->roles, true) && current_user_can('translate')) {
--- gravity/filtri campi form/Regex campo email su function.php
+++ gravity/filtri campi form/Regex campo email su function.php
@@ -1,6 +1,6 @@
 // filtro mail con lista domini gestiti da regex
 
-add_filter( 'gform_field_validation_4_37', 'validate_email', 10, 4 );
+add_filter( 'gform_field_validation_4_37', 'validate_email', 10, 4 );  /* 4_37 corrisponde a ID 4 e field 37 */ 
 function validate_email( $result, $value, $form, $field ) {
 	$pattern = "/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(arcese.com|ventanaserra.com|willbecreative.com|arcese+-+cosulich.ie)$/";
 	if ( $field->type == 'email' && $field->emailFormat != 'standard' && ! preg_match( $pattern, $value ) ) {
