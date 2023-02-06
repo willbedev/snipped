02d7c7a7e3ec77f9443afb1e689d8fd3e6c0f142
MarcoWillbe <marco@willbecreative.com>
2023-02-06 16:34:56 +0100

    blocco preventivo script iubenda

---
+++ iubenda_snippet/blocco script/blocco-script-matomo.php
@@ -0,0 +1,20 @@
+<!-- inserire sempre nello script l'attributo type="text/plain" e il tipo di cookies -->
+
+
+<!-- Matomo Tag Manager -->
+<script type="text/plain" class="_iub_cs_activate-inline" data-iub-purposes="4">
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='https://www.salavianelli.it/wp-content/plugins/matomo/app/../../../uploads/matomo/container_br4251XL.js'; s.parentNode.insertBefore(g,s);
+</script>
+<!-- End Matomo Tag Manager -->
+
+<!-- Matomo -->
+<script type="text/plain" class="_iub_cs_activate-inline" data-iub-purposes="4">
+var _paq = window._paq = window._paq || [];
+_paq.push(['enableJSErrorTracking']);
+_paq.push(['trackAllContentImpressions']);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);_paq.push(['alwaysUseSendBeacon']);_paq.push(['setTrackerUrl', "\/\/www.salavianelli.it\/wp-content\/plugins\/matomo\/app\/matomo.php"]);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.src="\/\/www.salavianelli.it\/wp-content\/uploads\/matomo\/matomo.js"; s.parentNode.insertBefore(g,s);
+</script>
+<!-- End Matomo Code -->
