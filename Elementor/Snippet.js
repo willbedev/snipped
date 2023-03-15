5754b3cc8cc0e8fe574b07816d2050cd5178282d
MarcoWillbe <marco@willbecreative.com>
2023-03-15 15:39:34 +0100

    elementor js

---
+++ Elementor/Snippet.js
@@ -0,0 +1,11 @@
+// Per chiudere di default l'accordion
+//Da inserire su file JSON
+// Per aprore tab specifici vedere guida https://element.how/elementor-open-specific-tab-toggle-accordion/
+
+jQuery(document).ready(function($) { 
+    
+    var delay = 100; setTimeout(function() {
+    $('.elementor-tab-title').removeClass('elementor-active');
+    $('.elementor-tab-content').css('display', 'none'); }, delay); 
+    
+});
