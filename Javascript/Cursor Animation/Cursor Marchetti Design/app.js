0fda3108f832f37dcc7b8f55ca0adadb77387c29
MarcoWillbe <marco@willbecreative.com>
2023-04-14 14:46:12 +0200

    modifica fiel js

--- Javascript/Cursor Animation/Cursor Marchetti Design/app.js
+++ Javascript/Cursor Animation/Cursor Marchetti Design/app.js
@@ -4,7 +4,8 @@
 
 let outline = document.querySelector('.outline');
 let cursor = document.querySelector('.cursor');
-// let a = document.querySelectorAll('a');
+// let a = document.querySelectorAll('a'); // variabile per tutti i link con a
+let titolo = document.querySelectorAll('h1'); // stesso esempio sopra con classe
 
 document.addEventListener('mousemove', function(e){  
     // viene creata variabile X
@@ -13,4 +14,17 @@ document.addEventListener('mousemove', function(e){
    
    outline.style.transform = `translate( calc(${x}px - 50%), calc(${y}px - 50%) )`;  // sovrascrive lo stile css
    cursor.style.transform = `translate( calc(${x}px - 50%) , calc(${y}px - 50%) )`;
+});
+
+
+titolo.forEach((item) => { //impostata la variabile a
+  item.addEventListener('mouseover', () => {
+    outline.classList.add('hover'); // aggiunge la classe hover 
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    outline.classList.remove('hover');  // toglie la classe hover 
+    cursor.classList.remove('hover');
+
+  });
 });
